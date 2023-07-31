13cb4b1d40e40647d92c3cfa295e7459e6125540
zhanglirong1999 <56445728+zhanglirong1999@users.noreply.github.com>
2023-07-31 11:14:10 +0800

    add BuildStrategy when open ir pass

--- static_and_dygraph/static_static.py
+++ static_and_dygraph/static_static.py
@@ -16,11 +16,18 @@ exe = paddle.static.Executor(place)
 # 开启compile program，ir pass优化：
 startup_program = paddle.static.default_startup_program()
 main_program = paddle.static.default_main_program()
-compiled_prog = fluid.CompiledProgram(main_program)
+
+build_strategy = paddle.static.BuildStrategy()
+# build_strategy.enable_addto = True
+build_strategy.fuse_elewise_add_act_ops = True
+
+main_program = paddle.static.CompiledProgram(
+        main_program, build_strategy=build_strategy
+    )
 exe.run(startup_program) 
 data_np = np.random.rand(1, 3, 32, 32).astype('float32')
-output = exe.run(compiled_prog, feed={'data': data_np}, fetch_list=[conv2d])
-print(output)
+output = exe.run(main_program, feed={'data': data_np}, fetch_list=[conv2d])
+print(main_program)
 
 
 # 不开启compile program，没有ir pass优化：
