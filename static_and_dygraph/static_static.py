a8c5b1984f290c43173282419329a051a7c32720
zhanglirong1999 <56445728+zhanglirong1999@users.noreply.github.com>
2023-07-31 09:37:15 +0800

    Update static_static.py, add ir pass or no ir pass

--- static_and_dygraph/static_static.py
+++ static_and_dygraph/static_static.py
@@ -6,16 +6,27 @@ paddle.enable_static()
 fluid.set_flags({'FLAGS_use_mkldnn': True})
 
 data = paddle.static.data(name='data', shape=[None, 3, 32, 32], dtype='float32')
+
 conv2d = paddle.static.nn.conv2d(input=data, num_filters=2, filter_size=3, act="relu")
 print(conv2d.shape) # [-1, 2, 30, 30]
 
 place = paddle.CPUPlace()  
-exe = paddle.static.Executor(place)  # 用Executor对象
+exe = paddle.static.Executor(place) 
 
+# 开启compile program，ir pass优化：
 startup_program = paddle.static.default_startup_program()
 main_program = paddle.static.default_main_program()
+compiled_prog = fluid.CompiledProgram(main_program)
 exe.run(startup_program) 
 data_np = np.random.rand(1, 3, 32, 32).astype('float32')
-output = exe.run(main_program, feed={'data': data_np}, fetch_list=[conv2d])
+output = exe.run(compiled_prog, feed={'data': data_np}, fetch_list=[conv2d])
 print(output)
+
+
+# 不开启compile program，没有ir pass优化：
+# startup_program = paddle.static.default_startup_program()
+# main_program = paddle.static.default_main_program()
+# exe.run(startup_program)
+# data_np = np.random.rand(1, 3, 32, 32).astype('float32')
+# output = exe.run(main_program, feed={'data': data_np}, fetch_list=[conv2d])
 
